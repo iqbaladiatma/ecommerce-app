864a754640eaaa3101c00b6a0b86c692fad431e1
iqbaladiatma <gazamuhammadadiatmav@gmail.com>
2025-08-26 08:49:04 +0700

    update bab 1 and the features is "login register submit button"

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+const primaryColor = Color(0xFF4C53A5);
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                _buildHeader(),
+                const SizedBox(height: 20),
+                _buildEmailField(),
+                const SizedBox(height: 20),
+                _buildPasswordField(),
+                const SizedBox(height: 20),
+                _buildLoginButton(),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+  Widget _buildHeader() {
+    return const Column(
+      children: [
+        Text(
+          'Welcome Back!',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: primaryColor,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Login To continue',
+          style: TextStyle(fontSize: 18, color: primaryColor),
+        ),
+      ],
+    );
+  }
+  Widget _buildEmailField() {
+    return TextFormField(
+      controller: _emailController,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        prefixIcon: const Icon(Icons.email, color: primaryColor,),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your email';
+        }
+        if (!RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").hasMatch(value)) {
+          return 'Format email tidak valid';
+        }
+        return null;
+      },
+    );
+  }
+  Widget _buildPasswordField() {
+    return TextFormField(
+      controller: _passwordController,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        prefixIcon: const Icon(Icons.lock, color: primaryColor,),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Password tidak boleh kosong';
+        }
+        if (value.length < 6) {
+          return 'Password harus 6 karakter';
+        }
+        return null;
+      },
+    );
+  }
+  Widget _buildLoginButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          Navigator.pushReplacementNamed(context, 'AccountPage');
+        },
+      },
+      style: ElevatedButton.styleFrom(
+          backgroundColor: primaryColor,
+          padding: const EdgeInsets.symetric(horizontal: 80, vertical: 15),
+          shape: RoundedRectangleBorder(BorderRadius.circular(30)),
+        ),
+      child: const Text('Login',
+      style: TextStyle(color: Colors.white, fontSize: 18),
+      ),
+    );
+  }
+  Widget _buildRegisterButton() {
+    return TextButton(
+      onPressed: () {
+        Navigator.pushNamed(context, '/');
+      },
+      child: const Text('Don\'t have an account? Register',
+      style: TextStyle(color: primaryColor),
+      ),
+    );
+  }
+}
